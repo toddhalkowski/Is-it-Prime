f51c1f27961b799bde3aaa67d135d6c5b93ff2b0
Todd Halkowski <thalkowski@virtusa.com>
2015-09-10 11:15:41 -0500

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,31 +1,62 @@
 //
 //  ViewController.swift
-//  Is it Prime
+//  Is It Prime
 //
-//  Created by todd d Halkowski on 6/3/15.
-//  Copyright (c) 2015 Geneva. All rights reserved.
+//  Created by Todd Halkowski
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var number: UITextField!
     
-    
-    @IBOutlet weak var numberguess: UITextField!
-    
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet var resultLabel: UILabel!
     
     @IBAction func buttonPressed(sender: AnyObject) {
         
-        var numberguessInt = numberguess.text.toInt()
+        var numberInt = number.text.toInt()
         
-        if numberguessInt = nil {
+        if numberInt != nil {
             
+            var unwrappedNumber = numberInt!
+            
+            var isPrime = true
+            
+            if unwrappedNumber == 1 {
+                
+                isPrime = false
+                
+            }
+            
+            if unwrappedNumber != 2 && unwrappedNumber != 1 {
+                
+                for var i = 2; i < unwrappedNumber; i++ {
+                    
+                    if unwrappedNumber % i == 0 {
+                        
+                        isPrime = false
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            if isPrime == true {
+                
+                resultLabel.text = "\(unwrappedNumber) is prime!"
+                
+            } else {
+                
+                resultLabel.text = "\(unwrappedNumber) is not prime!"
+                
+            }
+
             
             
         } else {
-           
+            
             resultLabel.text = "Please enter a number in the box"
             
         }
